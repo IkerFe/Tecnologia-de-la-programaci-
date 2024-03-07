0c97eaa220352220ef237353b5944195bce5a0fd
Iker Fer <iker.fernandez@enti.cat>
2024-03-07 11:26:40 +0100

    Sorting better

--- Tecno Programa/Tecno Programa/Sorting.cpp
+++ Tecno Programa/Tecno Programa/Sorting.cpp
@@ -30,16 +30,43 @@ void selectionSort(std::vector<int>& v)
 	}
 }
 
+void insertionSort(std::vector<int>& v2) 
+{
+	for (int i = 1; i < v2.size(); i++) 
+	{
+		//Element to evaluate
+		int x = v2[i];
+		//New index, used for comparing previous ordered elements
+		int j = i;
+		while (j > 0 && v2[j - 1]>x)
+		{
+			v2[j] = v2[j - 1];
+			--j;
+		}
+		v2[j] = x;
+	}
+}
 
 int main()
 {
 	//SORTING ALGORITHMS
 	// 3 10 7 -1 0
-	std::vector<int> v = { 3, -1,10, 200 };
+	std::vector<int> v = { 3, -1,10, 5 };
 	selectionSort(v);
 	for (int i = 0; i < v.size(); i++)
 	{
 		std::cout << v[i] << " ";
 	}
 	 std::cout<<std::endl;
+
+	 std::cout << "INSERTION SORT RESULT" << std::endl;
+	 std::vector<int> v2 = { 999, -3,33,5,-25, 200 };
+	 insertionSort(v2);
+	 for (int i = 0; i < v2.size(); i++)
+	 {
+		 std::cout << v2[i] << " ";
+	 }
+	 std::cout << std::endl;
 }
+
+
