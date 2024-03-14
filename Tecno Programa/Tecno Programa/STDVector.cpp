e2d8e793407a6b21f5304a1abbdd12d783d99bf4
Iker Fer <iker.fernandez@enti.cat>
2024-03-14 10:57:06 +0100

    STDVector

--- Tecno Programa/Tecno Programa/STDVector.cpp
+++ Tecno Programa/Tecno Programa/STDVector.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+/*#include <iostream>
 #include <vector>
+#include <algorithm>
 
 struct Student
 {
@@ -42,14 +43,34 @@ void createEnemies(int n)
 
 int main()
 {
-	std::vector<int>myIntVector = { 10,5,10,10 };
+	//CREATE
+	std::vector<int>myIntVector = { 10,99,7,3 };
 	std::vector<float> myFloatVector(10, -3.4f);
 	std::vector<int>myIntVector3(myIntVector);
 
 	std::vector<Student>myStudent;
 	Student s1(233543);
 	myStudent.push_back(s1);
+
 	//READ
+	//Iterate using iterators
+	std::cout << "Printing iterators" << std::endl;
+	std::vector<int>::iterator it = myIntVector.begin();
+	it++;
+	it = myIntVector.insert(it,5);
+	std::sort(myIntVector.begin(), myIntVector.end()
+	);
+	for (;it < myIntVector.end(); it++) 
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "---------------------" << std::endl;
+
+	for (int elem : myIntVector) 
+	{
+		std::cout << elem << " ";
+	}
 	for (int i = 0; i < myStudent.size(); ++i)
 	{
 		std::cout << myStudent[i].m_DNI << " " << myStudent[i].m_name;
@@ -76,4 +97,37 @@ int main()
 
 	//DELETE
 	myFloatVector.pop_back();
+}*/
+
+#include <iostream>
+
+#define A 10
+#define B 5
+#define max(a,b) ((a>b) ? a:b)
+
+int maxWithoutDefine(int a, int b)
+{
+	return(a > b) ? a : b;
+}
+
+/*int foo()
+{
+	//int A = 90;
+}*/
+
+
+int main()
+{
+	const int C = 3;
+	int result = A + B;
+	std::cout << result << std::endl;
+
+	result = B * A;
+
+	result = C * A;
+
+	int x = 7;
+	int y = 3;
+	std::cout << ++max(x, y) << std::endl;
+	std::cout << maxWithoutDefine(++x, ++y) << std::endl;
 }
