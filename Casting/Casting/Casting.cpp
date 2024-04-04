328048597a1f69cba615bd1b1118077c3832da77
Iker Fer <iker.fernandez@enti.cat>
2024-04-04 12:05:13 +0200

    Sorting

--- Casting/Casting/Casting.cpp
+++ Casting/Casting/Casting.cpp
@@ -8,19 +8,61 @@ enum class CarType
     MOTOROLA,
     TOYOTA
 };
+
+struct Fruit {
+    std::string name;
+    int size;
+
+    Fruit() 
+    {
+        name = "Invalid";
+        size = 0;
+    }
+};
+
+struct Vegetables {
+    std::string name;
+    int size;
+    int calories;
+
+    Vegetables()
+    {
+        name = "Invalid Vegetables";
+        size = 0;
+        calories = 0;
+    }
+};
 int main()
 {
     //C_style casting -> Not recommended
     float myFloat = 1.5476f;
     int myInt = (int)myFloat;
     int myInt2 = int(myFloat);
-    //static_cast
+
+    //C ++ static_cast
     float myFloat2 = 1.5476f;
     int myInt3 = static_cast<int>(myFloat);
 
-    int i = static_cast<int>(CarType::TOYOTA);
-    int i2 = (int)(CarType::TOYOTA);
+    int i = static_cast<int>(CarType::TOYOTA);//C++
+    int i2 = (int)(CarType::TOYOTA);//C
+    //C-style cast vs static_cast
+    char c = 'a';// 1 byte
+    //int* p = static_cast<int*>(&c);
     //const_cast
-    //reinterpret_cast
-}
+    Fruit f;//Stack
 
+    const Fruit* f1 = new Fruit();//Heap
+    //f1->name = "Peru";
+    Fruit* nonConstFruit = const_cast<Fruit*>(f1);
+    nonConstFruit->name = "Peru";
+
+    //reinterpret_cast
+    int* pointer = nullptr;// Se guardan direcciones de memoria
+    int value = 5; //Se guardan enteros
+    pointer = &value;
+    std::cout << *pointer<< std::endl;
+
+    Fruit* fruta1 = new Fruit();
+    Vegetables* vegetables1 = new Vegetables();
+
+}
---
+++ Sorting/Sorting/Sorting.cpp
@@ -0,0 +1,66 @@
+// Sorting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool compareFunc(int& a, int& b)
+{
+    return a > b;
+}
+
+bool compareSize(std::string& a, std::string& b)
+{
+    return a.size() < b.size();
+}
+
+struct Person
+{
+    std::string name;
+    int age;
+};
+
+bool comparePersonNameAndAge(const Person &a, const Person &b)
+{
+    //caso 1: Por nombre
+    if (a.name != b.name)
+        return a.name < b.name;
+
+    return a.age < b.age;
+
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::vector<int> v = { 10,25,3,35 };
+    std::sort(v.begin(), v.end(), compareFunc);
+
+    std::vector<std::string> words = { "Zebra", "Patata", "Venezuela", "Ola" };
+    std::sort(words.begin(), words.end(), compareSize);
+
+    std::vector<Person> people = { {"A",30}, {"Ba", 25}, {"C",35}, {"B",5} };
+    std::sort(people.begin(), people.end(), comparePersonNameAndAge);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        std::cout << words[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < people.size(); i++)
+    {
+        std::cout << people[i].name << " " << people[i].age << " ";
+    }
+    std::cout << std::endl;
+}
+
